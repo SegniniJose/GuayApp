ad989f23029a6654c72009712f0d30abee51865c
mayrapina2021 <nyary4040@gmail.com>
2026-07-01 18:53:59 -0500

    fix: point all base URLs to Render and export as release preset

--- godot/gd/Globals.gd
+++ godot/gd/Globals.gd
@@ -6,7 +6,7 @@ static var headers: Array[String] = ["Content-Type: application/json"]
 #change this URL to be the actual server
 #static var base_url_dev = "https://d583b40b-1cae-452e-ac12-74ac5903d7b0-00-1enao8eqex4ts.worf.replit.dev"
 #static var base_url_prod = "https://d583b40b-1cae-452e-ac12-74ac5903d7b0-00-1enao8eqex4ts.worf.replit.dev"
-static var base_url_dev = "http://localhost:9999"
+static var base_url_dev = "https://guayapp.onrender.com"
 static var base_url_prod = "https://guayapp.onrender.com"
 static var users_suggestions_limit = "5"
 static var base_url
